32a2cbd8a4fcf36eb77767b38c291ad5971a3796
ElvyKamande <elvy.waithera@student.moringaschool.com>
2025-05-02 08:59:21 +0300

    Atleast one test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,5 +2,10 @@ import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 import App from "../App";
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    // Act
+    // Assert
+  });
 
 // Your tests here
